253e59e84e1f2e7dddcd2f801c2f1fd37c080431
Benjamin Shockley <benjaminshockley@hotmail.com>
2019-12-19 10:07:26 -0600

    Update i2cencoderlibv21.py

--- i2cencoderlibv21.py
+++ i2cencoderlibv21.py
@@ -212,9 +212,15 @@ class I2CEncoderLibV21:
         """Reset the encoder."""
         self._write_reg(REG_GCONF, RESET)
 
+    def _event_caller(self, event) :
+        """ Call che attached callaback if it is defined."""
+        self._read_reg(REG_GCONF)
+        if event:
+            event()
+
     # Return true if the status of the encoder changed, otherwise return false #
     def update_status(self) :
-        """Runs the implemnted callbacks based on encoder status."""
+        """Runs the callback for the encoder status."""
         self.stat = self._read_reg(REG_ESTATUS)[0]
 
         if self.stat == 0:
@@ -222,37 +228,29 @@ class I2CEncoderLibV21:
             return False
 
         if (self.stat & PUSHR) != 0 :
-            self.onButtonRelease()
+            self._event_caller (self.onButtonRelease)
 
         if (self.stat & PUSHP) != 0 :
-            self.onButtonPush()
+            self._event_caller (self.onButtonPush)
 
         if (self.stat & PUSHD) != 0 :
-            self.onButtonDoublePush()
+            self._event_caller (self.onButtonDoublePush)
 
         if (self.stat & RINC) != 0 :
-            if self.onIncrement:
-                self.onIncrement()
-            if self.onChange:
-                self.onChange()
+            self._event_caller (self.onIncrement)
+            self._event_caller (self.onChange)
 
         if (self.stat & RDEC) != 0 :
-            if self.onDecrement:
-                self.onDecrement()
-            if self.onChange:
-                self.onChange()
+            self._event_caller (self.onDecrement)
+            self._event_caller (self.onChange)
 
         if (self.stat & RMAX) != 0 :
-            if self.onMax:
-                self.onMax()
-            if self.onMinMax:
-                self.onMinMax()
+            self._event_caller (self.onMax)
+            self._event_caller (self.onMinMax)
 
         if (self.stat & RMIN) != 0 :
-            if self.onMin:
-                self.onMin()
-            if self.onMinMax:
-                self.onMinMax()
+            self._event_caller (self.onMin)
+            self._event_caller (self.onMinMax)
 
         if (self.stat & INT_2) != 0 :
             self.stat2 = self._read_reg(REG_I2STATUS)[0]
@@ -261,27 +259,26 @@ class I2CEncoderLibV21:
                 return True
 
             if (self.stat2 & GP1_POS) != 0 :
-                self.onGP1Rise()
+                self._event_caller (self.onGP1Rise)
 
             if (self.stat2 & GP1_NEG) != 0 :
-                self.onGP1Fall()
+                self._event_caller (self.onGP1Fall)
 
             if (self.stat2 & GP2_POS) != 0 :
-                self.onGP2Rise()
+                self._event_caller (self.onGP2Rise)
 
             if (self.stat2 & GP2_NEG) != 0 :
-                self.onGP2Fall()
+                self._event_caller (self.onGP2Fall)
 
             if (self.stat2 & GP3_POS) != 0 :
-                self.onGP3Rise()
+                self._event_caller (self.onGP3Rise)
 
             if (self.stat2 & GP3_NEG) != 0 :
-                self.onGP3Fall()
+                self._event_caller (self.onGP3Fall)
 
             if (self.stat2 & FADE_INT) != 0 :
-                self.onFadeProcess()
+                self._event_caller (self.onFadeProcess)
         return True
-
 
     # Read functions #
 
